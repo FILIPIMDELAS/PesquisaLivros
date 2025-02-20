ff932b9b8c065c13216ed8e361e8ad677e736c72
FILIPIMDELAS <filipecristovam2020@gmail.com>
2025-02-20 19:25:43 -0300

    add style cart

--- src/pages/cart/cart.jsx
+++ src/pages/cart/cart.jsx
@@ -1,10 +1,27 @@
 import style from './Cart.module.css'
 import CompLayout from '../../components/ConpLayout/CompLayout'
+import { useGlobalContext } from '../../hooks/globalContext'
 
 export default function Cart() {
+  const { cardItens } = useGlobalContext()
+
   return (
    <CompLayout>
-
+    <div className={style.divContentAllCart}>
+      <div className={style.divContentCardCEP}>
+        <div className={style.divContentItensCard}>
+          <p>itens</p>
+        </div>
+        <div className={style.divCEPInfo}>
+          <p>cep</p>
+        </div>  
+      </div>
+      <div className={style.divResumeValue}>
+        <p>SubTotal: {}</p>
+        <p>Valor Frete: {}</p>
+        <p>Total: {}</p>
+      </div>
+    </div>
    </CompLayout>
   )
 }
